8877c03ef6e747e736a8402987bcc1b89537916d
Frank Anthony <frankez478@gmail.com>
2025-06-26 02:55:49 -0700

    Removed deprecated code

--- bot/services/data/db.js
+++ bot/services/data/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connection = await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.error("Database connection error:", error);
         process.exit(1);
